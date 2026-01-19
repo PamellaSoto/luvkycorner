afaae6c7c90b2123c0f07d4c5f96a00480ecd0de
Pamella Sotomayor <pamellasoto.dev@gmail.com>
2026-01-19 14:32:18 -0300

    fix(router): broken paths

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,27 @@
+import { Link } from "react-router-dom";
+
 function Button({ icon, label, goTo }) {
   const Icon = icon;
+  const isExternal = goTo.startsWith("http") || goTo.startsWith("mailto:");
+  const classes = "w-full md:w-auto content-center flex gap-3 px-4 py-2 items-center cursor-pointer rounded-full bg-[#6D37BE] text-white transition-all trainsition-duration-200 text-md"
+
+  if (isExternal) {
+    return (
+      <a
+        href={goTo}
+        target="_blank"
+        rel="noreferrer"
+        className={classes}
+      >
+        <Icon /> {label}
+      </a>
+    );
+  }
+
   return (
-    <a 
-      href={goTo}
-      className={`w-full md:w-auto content-center flex gap-3 px-4 py-2 items-center 
-      cursor-pointer rounded-full bg-[#6D37BE] text-amber-50
-      transition-all trainsition-duration-200
-      text-md`}>
+    <Link to={goTo} className={classes}>
       <Icon /> {label}
-    </a>
+    </Link>
   )
 }
 
